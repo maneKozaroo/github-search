f6131cecaf95928c1abaf56a281709c72066268d
maneKozaroo <j.zudell@joyz.co.jp>
2022-09-04 14:13:08 +0900

    feat: add a search input component

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import router from "./router";
 
 import App from "./App.vue";
 import InputBase from "./components/input/InputBase.vue";
+import SearchInput from "./components/GithubSearch/SearchInput.vue";
 
 import "./assets/main.css";
 
@@ -14,4 +15,6 @@ app.use(createPinia());
 app.use(router);
 
 app.component("InputBase", InputBase);
+app.component("SearchInput", SearchInput);
+
 app.mount("#app");
