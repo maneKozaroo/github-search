c3a34dc73f4953ba93bdc8bd32f937911dd4a3c7
maneKozaroo <j.zudell@joyz.co.jp>
2022-09-04 14:14:15 +0900

    feat: add a search form component

---
+++ src/components/GithubSearch/SearchForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <form
+    :id="FORMS.SEARCH.NAME"
+    :name="FORMS.SEARCH.NAME"
+    @submit.prevent="handleSearchSubmit"
+  >
+    <SearchInput type="search" />
+  </form>
+</template>
+
+<script setup lang="ts">
+import { FORMS } from "@/constants";
+
+const handleSearchSubmit = (event: Event) => {
+  const targetForm = event.target as HTMLFormElement;
+
+  if (!targetForm) return;
+
+  const formData = new FormData(targetForm);
+
+  console.log(
+    "TODO: call search action with text:",
+    formData.get(FORMS.SEARCH.FIELDS.QUERY.NAME)
+  );
+};
+</script>
+
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import router from "./router";
 
 import App from "./App.vue";
 import InputBase from "./components/input/InputBase.vue";
+import SearchForm from "./components/GithubSearch/SearchForm.vue";
 import SearchInput from "./components/GithubSearch/SearchInput.vue";
 
 import "./assets/main.css";
@@ -15,6 +16,7 @@ app.use(createPinia());
 app.use(router);
 
 app.component("InputBase", InputBase);
+app.component("SearchForm", SearchForm);
 app.component("SearchInput", SearchInput);
 
 app.mount("#app");
