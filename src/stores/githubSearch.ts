dcbb815cbddb10ca20e2b9f4165654cdce4d0770
maneKozaroo <j.zudell@joyz.co.jp>
2022-09-11 23:58:54 +0900

    refactor: alter pinia store structure to allow throttling

--- src/stores/githubSearch.ts
+++ src/stores/githubSearch.ts
@@ -1,28 +1,111 @@
 import { computed, reactive } from "vue";
 import { defineStore } from "pinia";
-
 import { apiService } from "@/services";
-import type { SearchState } from "@/types";
+import { throttle } from "@/utils";
+import type { SearchFunction, SearchState } from "@/types";
 
 export const useGithubSearchStore = defineStore("githubSearch", () => {
-  const search = reactive<SearchState>({
-    pageNumber: 1,
+  const initialState = (): SearchState => ({
+    form: {
+      userInput: "",
+    },
+    status: {
+      currentPage: 1,
+      loading: false,
+    },
     results: undefined,
+    throttling: {
+      timeframe: 60000,
+      requestLimit: 10,
+      requestTimes: [],
+    },
   });
 
-  async function searchRepositories(userInput: string) {
-    try {
-      const currentPage = search.pageNumber;
+  /**
+   * State
+   */
+  const search = reactive(initialState());
 
-      const queryString = `page=${currentPage}&q=${userInput}`;
+  /**
+   * Actions
+   */
+  const reset = () => {
+    Object.assign(search, initialState());
+  };
 
-      const results = await apiService.github.repositories.search(queryString);
+  const throttledSearch = throttle<SearchFunction>({
+    ...search.throttling,
+    throttledFunction: async (page: number, queryString: string) => {
+      try {
+        const queryComponent = `page=${page}&q=${queryString}`;
 
-      search.results = results;
-    } catch (error) {
-      console.log(error);
+        search.status.loading = true;
+
+        const results = await apiService.github.repositories.search(
+          queryComponent
+        );
+
+        search.results = results;
+      } catch (error) {
+        console.log(error);
+      } finally {
+        search.status.loading = false;
+      }
+    },
+  });
+
+  const setCurrentPage = ({ currentPage }: { currentPage: number }) => {
+    search.status.currentPage = currentPage;
+  };
+
+  const setUserInput = ({ userInput }: { userInput: string }) => {
+    search.form.userInput = userInput;
+  };
+
+  /**
+   * Getters
+   */
+  const paginationPages = computed(() => {
+    const pageCount = totalPages.value;
+    const desiredLinkPadding = 5;
+    const desiredLinkCount = desiredLinkPadding * 2 + 1;
+
+    if (pageCount === 0) return null;
+
+    if (pageCount <= desiredLinkCount) {
+      return Array.from(Array(pageCount), (_, index) => {
+        return index + 1;
+      });
     }
-  }
+
+    const desiredLeft = search.status.currentPage - desiredLinkPadding;
+    const desiredRight = search.status.currentPage + desiredLinkPadding;
+
+    const leftOverflowed = desiredLeft < 1;
+    const rightOverflowed = desiredRight > pageCount;
+
+    const leftAddition = rightOverflowed ? desiredRight - pageCount : 0;
+    const rightAddition = leftOverflowed ? Math.abs(desiredLeft) + 1 : 0;
+
+    const totalLeft = desiredLeft - leftAddition;
+    const totalRight = desiredRight + rightAddition;
+
+    const left = totalLeft < 1 ? 1 : totalLeft;
+    const right = totalRight > pageCount ? pageCount : totalRight;
+
+    const pages: number[] = [];
+    for (let i = left; i <= right; i += 1) {
+      pages.push(i);
+    }
+
+    return pages;
+  });
+
+  const throttled = computed(() => {
+    return (
+      search.throttling.requestTimes.length >= search.throttling.requestLimit
+    );
+  });
 
   const totalPages = computed(() => {
     const itemsPerPage = 30;
@@ -32,8 +115,18 @@ export const useGithubSearchStore = defineStore("githubSearch", () => {
   });
 
   return {
+    // State
     search,
-    searchRepositories,
+
+    // Actions
+    reset,
+    setCurrentPage,
+    setUserInput,
+    throttledSearch,
+
+    // Getters
+    paginationPages,
+    throttled,
     totalPages,
   };
 });
--- src/types/githubSearchStore.ts
+++ src/types/githubSearchStore.ts
@@ -1,6 +1,18 @@
 import type { SearchResponse } from "./api";
 
+export type SearchFunction = (page: number, queryString: string) => void;
 export interface SearchState {
-  pageNumber: number;
+  form: {
+    userInput: string;
+  };
+  status: {
+    currentPage: number;
+    loading: boolean;
+  };
   results?: SearchResponse;
+  throttling: {
+    timeframe: number;
+    requestLimit: number;
+    requestTimes: number[];
+  };
 }
